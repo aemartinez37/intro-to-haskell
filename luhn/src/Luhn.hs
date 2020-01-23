29e1de102c88b7bebb304109773abdc2ccf6015f
Juan Pedro Villa Isaza <584947+jpvillaisaza@users.noreply.github.com>
2020-01-23 15:35:13 -0500

    Add luhn template and test

--- luhn/src/Luhn.hs
+++ luhn/src/Luhn.hs
@@ -1,1 +1,83 @@
+----------------------------------------------------------------------
+-- |
+--
+-- The Luhn algorithm
+--
+-- This is a template to solve CIS 194 (Spring 2013): Homework 1,
+-- exercises 1–4 (validating credit card numbers).
+--
+-- https://www.cis.upenn.edu/~cis194/spring13/hw/01-intro.pdf
+--
+-- To work on your solution, run:
+--
+--   stack ghci luhn:lib luhn:test:spec
+--
+-- Then, to reload your changes, run:
+--
+--   > :reload
+--
+-- Or:
+--
+--   > :r
+--
+-- And, to test your solution, run:
+--
+--   > main
+--
+----------------------------------------------------------------------
+
 module Luhn where
+
+----------------------------------------------------------------------
+-- Exercise 1
+----------------------------------------------------------------------
+
+-- |
+--
+-- >>> toDigits 1234
+-- [1,2,3,4]
+
+toDigits :: Integer -> [Integer]
+toDigits = undefined
+
+----------------------------------------------------------------------
+-- Exercise 2
+----------------------------------------------------------------------
+
+-- |
+--
+-- >>> doubleEveryOther [8, 7, 6, 5]
+-- [16,7,12,5]
+--
+-- >>> doubleEveryOther [1, 2, 3]
+-- [1,4,3]
+
+doubleEveryOther :: [Integer] -> [Integer]
+doubleEveryOther = undefined
+
+----------------------------------------------------------------------
+-- Exercise 3
+----------------------------------------------------------------------
+
+-- |
+--
+-- >>> sumDigits [16, 7, 12, 5]
+-- 22
+
+sumDigits :: [Integer] -> Integer
+sumDigits = undefined
+
+----------------------------------------------------------------------
+-- Exercise 4
+----------------------------------------------------------------------
+
+-- |
+--
+-- >>> validate 4012888888881881
+-- True
+--
+-- >>> validate 4012888888881882
+-- False
+
+validate :: Integer -> Bool
+validate = undefined
---
+++ luhn/test/Main.hs
@@ -0,0 +1,31 @@
+module Main (main) where
+
+-- hspec
+import qualified Test.Hspec as Hspec
+
+-- luhn
+import qualified Luhn
+
+main :: IO ()
+main =
+  Hspec.hspec $ do
+    Hspec.describe "toDigits" $ do
+      Hspec.it "splits numbers in digits when the given number is positive" $
+        Luhn.toDigits 1234 `Hspec.shouldBe` [1, 2, 3, 4]
+      Hspec.it "returns an empty list when the given number is zero" $
+        Luhn.toDigits 0 `Hspec.shouldBe` []
+      Hspec.it "returns an empty list when the given number is negative" $
+        Luhn.toDigits (-17) `Hspec.shouldBe` []
+    Hspec.describe "doubleEveryOther" $ do
+      Hspec.it "doubles every other number when there is an even number of numbers" $
+        Luhn.doubleEveryOther [8, 7, 6, 5] `Hspec.shouldBe` [16, 7, 12, 5]
+      Hspec.it "doubles every other number when there's an odd number of numbers" $
+        Luhn.doubleEveryOther [1, 2, 3] `Hspec.shouldBe` [1, 4, 3]
+    Hspec.describe "sumDigits" $
+      Hspec.it "sums all digits" $
+        Luhn.sumDigits [16, 7, 12, 5] `Hspec.shouldBe` 22
+    Hspec.describe "validate" $ do
+      Hspec.it "succeeds for a valid credit card number" $
+        Luhn.validate 4012888888881881 `Hspec.shouldBe` True
+      Hspec.it "fails for an invalid credit card number" $
+        Luhn.validate 4012888888881882 `Hspec.shouldBe` False
